46fb5ea871fe92526a4edf7cacb731bf33001ed8
AhmadNoor4050 <cls>
2023-09-21 18:40:06 +0500

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Script for navigation bar
+let bar = document.getElementById('bar')
+let close = document.getElementById('close')
+let navbar = document.getElementById('navbar')
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    navbar.classList.add('active')
+  })
+}
+
+if (close) {
+  close.addEventListener('click', () => {
+    navbar.classList.remove('active')
+  })
+}
